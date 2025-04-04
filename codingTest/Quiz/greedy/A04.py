aecdb4cb4a09c53518229f27a3c6cb9fe19bca2d
gongbu22 <pyujin0711@naver.com>
2025-04-04 22:57:18 +0900

    그리디 - 문자열 뒤집기, 만들 수 없는 금액액

---
+++ codingTest/Quiz/greedy/A04.py
@@ -0,0 +1,12 @@
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+target = 1
+for i in data:
+    # 만들 수 없는 금액을 찾았을 때 반복 종료
+    if target < i:
+        break
+    target += i
+
+print(target)
